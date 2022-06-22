09e860e52b391c77bb9c21db3b963763b80165b3
Vinícius Neves <vinilneves@gmail.com>
2022-06-22 00:55:54 -0300

    Corrigindo o bug de altura do editor

--- frontend/src/pages/editor/index.jsx
+++ frontend/src/pages/editor/index.jsx
@@ -19,12 +19,12 @@ const Row = styled.div`
   justify-content: center;
   gap: 1px;
   background: grey;
-  width: ${props => props.width || 'inherit' };
+  width: ${props => props.width || 'inherit'};
   margin: 0 auto;
 `
 
 const Column = styled.div`
-  flex-grow: ${props => props.flexGrow || 1 };
+  flex-grow: ${props => props.flexGrow || 1};
 `
 
 const Message = styled.div`
@@ -44,87 +44,94 @@ const ErrorMessage = styled(Message)`
 `
 
 const Editor = (props) => {
-    const [requests, setRequests] = useState({})
-    const [editorContent, setEditorContent] = useState('')
-    const [YAMLIsValid, setYAMLIsValid] = useState(true)
+  const [requests, setRequests] = useState({})
+  const [editorContent, setEditorContent] = useState('')
+  const [YAMLIsValid, setYAMLIsValid] = useState(true)
+  const [editorLoaded, setEditorLoaded] = useState(false)
 
-    useEffect(() => {
-        const fetchRequests = async () => {
-            const requests = await requestService.list()
+  useEffect(() => {
+    const fetchRequests = async () => {
+      const requests = await requestService.list()
 
-            setRequests({ requests })
-            setEditorContent(YAML.stringify({ requests }))
-        }
-
-        fetchRequests()
-    }, [])
-
-    const onEditorContentChange = (value) => {
-        setEditorContent(value)
-
-        try {
-            let parsedYAML = YAML.parse(value)
-            setYAMLIsValid(true)
-            setRequests(parsedYAML)
-        } catch (e) {
-            setYAMLIsValid(false)
-        }
+      setRequests({requests})
+      setEditorContent(YAML.stringify({requests}))
     }
 
-    return (
-        <>
-            { YAMLIsValid ?
-                <SuccessMessage>YAML Válido.</SuccessMessage> :
-                <ErrorMessage>YAML Inválido.</ErrorMessage>
-            }
-            <Row>
-                <Column style={{ flexGrow: 2 }}>
-                    <div style={{ fontWeight: 'bold', background: '#282c34', color: 'white', fontSize: '1.5em' }}>
-                        Definições
-                    </div>
-                    <ReactAce
-                      width='100%'
-                      height='100%'
-                      value={editorContent}
-                      onChange={onEditorContentChange}
-                      mode='yaml'
-                      theme='one_dark'
-                    />
-                </Column>
-                <Column>
-                    <div style={{ fontWeight: 'bold', background: 'white', color: '#282c34', fontSize: '1.5em' }}>
-                        Requests
-                    </div>
-                    { requests?.requests && requests.requests.map(request => (
-                        <div style={{ borderBottom: '1px solid gray' }}>
-                            <div style={{ textAlign: 'left', background: 'white', padding: '2em' }}>
-                                <h1>{ request.method }</h1>
-                                <h2>{ request.path }</h2>
-                                { request?.samples && request.samples.map(sample => (
-                                  <>
-                                      <h3>{ sample.name }</h3>
-                                      <h4>{ sample.description }</h4>
-                                      <h3>Headers</h3>
-                                      { sample.headers && (
-                                        <p><pre>{ JSON.stringify(sample.headers) }</pre></p>
-                                      ) }
-                                      <h3>Params</h3>
-                                      { sample.params && (
-                                        <p><pre>{ JSON.stringify(sample.params, null, 2) }</pre></p>
-                                      ) }
-                                      <h3>Response</h3>
-                                      { sample.response && (
-                                        <p><pre>{ JSON.stringify(sample.response, null, 2) }</pre></p>
-                                      ) }
-                                  </>
-                                )) }
-                            </div>
-                        </div>
-                    )) }
-                </Column>
-            </Row>
-        </>
-    )
+    fetchRequests()
+  }, [])
+
+  const onEditorContentChange = (value) => {
+    setEditorContent(value)
+
+    try {
+      let parsedYAML = YAML.parse(value)
+      setYAMLIsValid(true)
+      setRequests(parsedYAML)
+    } catch (e) {
+      setYAMLIsValid(false)
+    }
+  }
+
+  return (
+    <>
+      {YAMLIsValid ?
+        <SuccessMessage>YAML Válido.</SuccessMessage> :
+        <ErrorMessage>YAML Inválido.</ErrorMessage>
+      }
+      <Row>
+        <Column style={{ position: 'relative' }}>
+          <div style={{ fontWeight: 'bold', background: '#282c34', color: 'white', fontSize: '1.5em' }}>
+            Definições
+          </div>
+          <ReactAce
+            width='100%'
+            height='800px'
+            value={editorContent}
+            onChange={onEditorContentChange}
+            mode='yaml'
+            theme='one_dark'
+          />
+        </Column>
+        <Column>
+          <div style={{fontWeight: 'bold', background: 'white', color: '#282c34', fontSize: '1.5em'}}>
+            Requests
+          </div>
+          {requests?.requests && requests.requests.map(request => (
+            <div style={{borderBottom: '1px solid gray'}}>
+              <div style={{textAlign: 'left', background: 'white', padding: '2em'}}>
+                <h1>{request.method}</h1>
+                <h2>{request.path}</h2>
+                {request?.samples && request.samples.map(sample => (
+                  <>
+                    <h3>{sample.name}</h3>
+                    <h4>{sample.description}</h4>
+                    <h3>Headers</h3>
+                    {sample.headers && (
+                      <p>
+                        <pre>{JSON.stringify(sample.headers)}</pre>
+                      </p>
+                    )}
+                    <h3>Params</h3>
+                    {sample.params && (
+                      <p>
+                        <pre>{JSON.stringify(sample.params, null, 2)}</pre>
+                      </p>
+                    )}
+                    <h3>Response</h3>
+                    {sample.response && (
+                      <p>
+                        <pre>{JSON.stringify(sample.response, null, 2)}</pre>
+                      </p>
+                    )}
+                  </>
+                ))}
+              </div>
+            </div>
+          ))}
+        </Column>
+      </Row>
+    </>
+  )
 }
 
 export default Editor
