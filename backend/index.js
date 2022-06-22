437d0e553576a519c6f3aca8f1369824961650f1
Vinícius Neves <vinilneves@gmail.com>
2022-06-22 00:27:28 -0300

    Mockando a primeira request a partir do json

--- backend/index.js
+++ backend/index.js
@@ -1,22 +1,25 @@
 const express = require('express')
+const cors = require('cors')
 
 const requests = require('./data/requests.json')
 
 const app = express()
-const port = 3000
+const port = 3001
 
 app.use(express.json())
+app.use(cors())
 
 app.get('/requests', (req, res) => {
     res.json(requests)
 })
 
 const parseIfString = (value) =>  {
-    if (typeof value === 'string') return value
+    if (typeof value !== 'string') return value
     if (value.toLowerCase() === 'true') return true
     if (value.toLowerCase() === 'false') return false
     if (value.match(/^\d+$/)) return Number.parseInt(value)
     if (value.match(/^\d+\.\d+$/)) return Number.parseInt(value)
+    return value
 }
 
 app.use((req, res) => {
